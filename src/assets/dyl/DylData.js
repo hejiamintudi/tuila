c7a2915c6322820beaedb1d385b67cf81916846c
hejiamintudi <2031862523@qq.com>
2019-09-16 09:30:25 +0800

    sdf

---
+++ src/assets/dyl/DylData.js
@@ -0,0 +1,1 @@
+window.dyl||(window.dyl={}),dyl._data={en:{en0:{time:3,id:"en0",hp:9,atk:2,skill:"not",_data:{}},en1:{time:3,id:"en1",hp:9,atk:2,skill:"not",_data:{}},en2:{time:6,id:"en2",hp:9,atk:-2,skill:"not",_data:{}},en3:{time:6,id:"en3",hp:9,atk:-2,skill:"not",_data:{}}},bullet:{one:{skill:"not",id:"one",coin:0,atk:1,_data:{}}}};
